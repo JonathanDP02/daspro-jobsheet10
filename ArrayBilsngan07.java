f2c293136c8c6a59b16a16ce956291da90edaf9e
JonathanDP02 <jonatanpangestu23@gmail.com>
2024-10-31 13:51:42 +0700

    pertanyaan 2

--- ArrayBilsngan07.java
+++ ArrayBilsngan07.java
@@ -4,18 +4,13 @@ public class ArrayBilsngan07 {
         
 //..................deklarasi array..............//
 
-        int [] bil = new int [4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        int [] bil = {5,13,-7,17};
 
 //..................output array.................//
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-        
+        for (int i = 0; i <= 4; i++){
+            System.out.println(bil[i]);
+        }
+
     }
 }
