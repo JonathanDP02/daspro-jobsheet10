0a698f9803b2fa4df5cd898e197dc055ecc00af6
JonathanDP02 <jonatanpangestu23@gmail.com>
2024-10-31 12:33:57 +0700

    Percobaan 1

---
+++ ArrayBilsngan07.java
@@ -0,0 +1,21 @@
+public class ArrayBilsngan07 {
+
+    public static void main(String[] args) {
+        
+//..................deklarasi array..............//
+
+        int [] bil = new int [4];
+        bil [0] = 5;
+        bil [1] = 13;
+        bil [2] = -7;
+        bil [3] = 17;
+
+//..................output array.................//
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+        
+    }
+}
